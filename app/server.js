edd09379ace1a22921902abafc68aab45e42744d
Groot-2001 <shivasilmawala841@gmail.com>
2023-12-25 23:59:04 +0530

    basic project setup done

---
+++ app/server.js
@@ -0,0 +1,20 @@
+//importing dependencies
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+//basic port setup
+const PORT = process.env.PORT || 5000;
+
+//setting up middlewares
+app.use(morgan("tiny"));
+
+//setting up with routes
+app.use("/", (req, res) => {
+  res.send("Welcome to the Movie App.");
+});
+
+//setting up with servers
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
